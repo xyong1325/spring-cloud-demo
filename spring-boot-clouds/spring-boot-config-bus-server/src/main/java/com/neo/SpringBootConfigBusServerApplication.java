acadf2e33b50786ec9b41bda24ca4a3a3afcccfa
xyong1325 <xyong1325@163.com>
2018-12-19 14:49:33 +0800

    spring cloud    ribbon    feign    hystrix    zuul    config    bus

---
+++ spring-boot-clouds/spring-boot-config-bus-server/src/main/java/com/neo/SpringBootConfigBusServerApplication.java
@@ -0,0 +1,19 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class SpringBootConfigBusServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigBusServerApplication.class, args);
+	}
+
+}
+
