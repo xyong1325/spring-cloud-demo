087cff7fc91192616d03fa47af5cc22e8fa5bc0c
xyong1325 <xyong1325@163.com>
2019-01-03 11:04:46 +0800

    spring-boot-quartz 定时任务，通过rabbit  发送接收消息

--- spring-boot-clouds/spring-boot-quartz/src/main/java/com/neo/rabbitmq/RabbitReceive.java
+++ spring-boot-clouds/spring-boot-quartz/src/main/java/com/neo/rabbitmq/RabbitReceive.java
@@ -1,11 +1,13 @@
 package com.neo.rabbitmq;
 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RabbitListener(queues="topic.message")
 public class RabbitReceive {
-    @RabbitListener(queues="topic.message")
+     @RabbitHandler
     public void process1(String str) {
         System.out.println("message:"+str);
     }
